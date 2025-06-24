8f6471dfbf624ef4a099f36c258f3ed29e3e0582
jojo-saturo <niyitripplejoejoejoe@gmail.com>
2025-06-24 15:28:24 +0100

    Simple Go cal

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var operation string
+	var num1, num2 float64
+
+	fmt.Print("Enter operation (+, -, *, /) ")
+	fmt.Scanln(&operation)
+
+	fmt.Print("Enter first number ")
+	fmt.Scanln(&num1)
+
+	fmt.Print("Enter second number ")
+	fmt.Scanln(&num2)
+
+	switch operation {
+	case "+":
+		fmt.Printf("Result: %g\n", num1+num2)
+	case "-":
+		fmt.Printf("Result: %g\n", num1-num2)
+	case "*":
+		fmt.Printf("Result: %g\n", num1*num2)
+	case "/":
+		if num2 != 0 {
+			fmt.Printf("Result: %g\n", num1/num2)
+		} else {
+			fmt.Println("Error: Division by zero")
+		}
+	default:
+		fmt.Println("Error: Invalid operation")
+	}
+}
